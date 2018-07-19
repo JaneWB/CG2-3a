180d150be900fc72b7e4071044d18c70e424ed39
JaneWB <brownjannick@att.net>
2018-07-18 20:14:27 -0500

    Three strings

--- CG2-3a/Program.cs
+++ CG2-3a/Program.cs
@@ -6,7 +6,24 @@ namespace CG2_3a
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?");
+            Console.WriteLine("Type your first name: ");
+            string myFirstName;
+            myFirstName = Console.ReadLine();
+
+
+
+            Console.WriteLine("Type your last name: ");
+            string myLastName = Console.ReadLine();
+
+            Console.WriteLine("What is your birthdate, month and day only");
+            Console.WriteLine("Type the month and day of your birthdate: ");
+            string birthdate = Console.ReadLine();
+
+
+
+            Console.WriteLine("Hello, " + myFirstName + " " + myLastName + ", your birthday is " + birthdate);
+            Console.ReadLine();
         }
     }
 }
